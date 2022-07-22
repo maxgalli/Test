f210a22227ddd89685ad2aece5f3c06a3c7995f1
qianying guo <qyguo@buaa.edu.cn>
2022-07-22 18:08:22 +0200

    test 2018

---
+++ fromDavid/PTH_Jul22_newBinning/ggH/changeName.sh
@@ -0,0 +1,16 @@
+Bin=(0_15 15_30 30_45 45_80 80_120 120_200 200_350 350_600 GT600)
+Path_="./"
+for final in 2e2mu 4mu 4e;
+do
+    for N in {0..8};
+    do
+        echo hzz4l_${final}S_13TeV_xs_bin${N}.txt, _recobin${N}
+        sed -i "8s/observation 2/observation -1/" ${Path_}/hzz4l_${final}S_13TeV_xs_bin${N}.txt
+        #sed -i "s/_recobin${N}/_${Bin[$N]}/g" ${Path_}/hzz4l_${final}S_13TeV_xs_bin${N}.txt 
+        #for i in {0..8};
+        #do
+        #    #echo ${i}, ${Bin[$i]}
+        #    sed -i "s/HBin${i}/H_PTH_${Bin[$i]}/g" ${Path_}/hzz4l_${final}S_13TeV_xs_bin${N}.txt
+        #done
+    done
+done
